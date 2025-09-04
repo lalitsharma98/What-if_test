182db9dead72f84189cf7744592a3fb6e0a6e3a3
Lalit sharma <32238406+lalitsee49@users.noreply.github.com>
2025-09-04 22:43:11 +0530

    new changes in app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from data_extractor import run_data_extractor
-from staffing_requirements import run_fte_analysis
+# from fte_analysis import run_fte_analysis
+# from staffing_Req_Ver3 import run_fte_analysis
 from default import main
-from Datamart_Daywise_V3 import run_daywise_tool_ver3
-import sys
-import os
+from Datamart_Daywise_V4 import run_daywise_tool_ver4
+from staffing_Req_Ver4 import run_fte_analysis_4
+
+
+# App title
+# st.set_page_config(page_title="What-if Analysis", layout="wide")
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
@@ -13,7 +17,6 @@ page = st.sidebar.radio("Go to", ["What-if analysis","Data Extractor", "Staffing
 # Main content based on selection
 if page == "What-if analysis":
     st.title("What-If Analysis Tool")
-    st.write("Select a scenario to run the analysis.")
     main()
 
 elif page == "Data Extractor":
@@ -22,9 +25,8 @@ elif page == "Data Extractor":
 
 elif page == "Staffing Req Analysis":
     st.title("Staffing Req Analysis")
-    run_fte_analysis()
+    run_fte_analysis_4()
 
 elif page == "Datamart-Daywise":
     st.title("Datamart Daywise")
-    run_daywise_tool_ver3()
-
+    run_daywise_tool_ver4()
